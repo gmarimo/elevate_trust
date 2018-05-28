dbb8c57b4f83f39757ad3a344b99e01f76ba1778
tinashediego <tinashediego@gmail.com>
2018-05-28 17:03:50 +0200

    B
    all changes

---
+++ src/pages/login/login.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProfilePage} from '../profile/profile';
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+  showprof(){
+    this.navCtrl.push(ProfilePage);
+  }
+
+}
